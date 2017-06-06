c7284fe59ee137a708814947bc06163cee6df55b
nedelman <nedelman@gmail.com>
2017-06-06 06:07:56 -0700

    Moved clean scripts.

---
+++ clean.sh
@@ -0,0 +1,16 @@
+rm -r cpp/test/project/linux-netbeans/bin
+rm -r cpp/test/project/linux-netbeans/build
+
+rm -r cpp/library/project/linux-netbeans/build
+rm -r cpp/library/project/linux-netbeans/dist
+
+rm -r cpp/library/project/ios-xcode/Build
+rm -r cpp/library/project/macos-xcode/Build
+rm -r cpp/library/project/tvos-xcode/Build
+
+rm -r cpp/library/project/ios-xcode/finjin-common-lib.xcodeproj/xcuserdata
+rm -r cpp/library/project/macos-xcode/finjin-common-lib.xcodeproj/xcuserdata
+rm -r cpp/library/project/tvos-xcode/finjin-common-lib.xcodeproj/xcuserdata
+
+rm -r cpp/assets/shaders/metal/output
+rm -r cpp/assets/shaders/vulkan/output
